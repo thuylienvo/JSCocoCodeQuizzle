6b627ec68f43408d1883db56f40e65aff8861088
Mimi Vo <mimzy414@gmail.com>
2021-04-15 15:21:50 -0500

    added Game start content

---
+++ assets/js/questions.js
@@ -0,0 +1,64 @@
+Which of the following is a valid type of function javascript supports?
+named function
+anonymous function 
+Both of the above
+None of the above
+
+JavaScript is a case-sensitive language
+
+True 
+False
+
+Which built-in method removes the last element from an array?
+last()
+get()
+pop()
+None of the above.
+
+Which of the following function of Array object returns true if at least one element in this array satisfies the provided testing function?
+
+reverse()
+shift()
+slice()
+some()
+
+Which of the following is true about variable naming conventions in JavaScript?
+
+You should not use any of the JavaScript reserved keyword as variable name.
+JavaScript variable names should not start with a numeral (0-9).
+Both of the above.
+None of the above.
+
+Which of the following function of Number object defines how many total digits to display of a number?
+
+toExponential()
+toFixed()
+toLocaleString()
+toPrecision()
+
+Which of the following function of String object returns the calling string value converted to upper case?
+
+toLocaleUpperCase()
+toUpperCase()
+toString()
+substring()
+
+
+Which of the following function of String object causes a string to be displayed in the specified color as if it were in a <font color='color'> tag?
+
+fixed()
+fontcolor()
+blink()
+bold()
+
+Can you pass a anonymous function as an argument to another function?
+
+true
+false
+
+Which built-in method returns the string representation of the number's value?
+
+toValue()
+toNumber()
+toString()
+None of the above.
