2bb5bd915da9b1645a7fea0e4a2e292c092454f4
Mimi Vo <mimzy414@gmail.com>
2021-04-20 12:17:04 -0500

    added score and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,39 @@
-let gameStart = document.querySelector('.btn')
+let gameStartEl = document.querySelector('game-start-btn');
+let highScoreEl = document.querySelector('highscore-btn');
+let timerEl = document.querySelector('#timer0');
+let scoreEl = document.querySelector('#score0')
+let timeSeconds = 60;
+let startScore = 60;
+
 
 //User selects Start to begin game 
-gameStart.attachEvent('onclick', function() {console.log(questions[0])});
+    //Timer will set off with score
+    scoreEl.innerHTML = `${timeSeconds}`;
+    timerEl.innerHTML = `00:${timeSeconds}`;
+    const countDown = setInterval (()=>{
+        timeSeconds--;
+        displayTime(timeSeconds);
+        if(timeSeconds <= 0 || timeSeconds <1){
+            clearInterval(countDown);
+        }
+    },1000)
+
+    function displayTime(second){
+        const min = Math.floor(second /60);
+        const sec = Math.floor(second % 60);
+        timerEl.innerHTML =`${min<10 ? '0': ''}${min}:${sec}`
+        scoreEl.innerHTML =`${sec}`
+    }
+    //And display question(s) and choice(s)
+    window.onload = function () {
+        console.log(questions[0])
+        document.getElementById("question").textContent=questions[0].question;
+        document.getElementById("0").textContent=questions[0].choices[0];
+        document.getElementById("1").textContent=questions[0].choices[1];
+        document.getElementById("2").textContent=questions[0].choices[2];
+        document.getElementById("3").textContent=questions[0].choices[3];
+
+    }
 
 //Game questions run 
 
