3355c690df2ac94ba7ad5134ead176c61b5ce7b2
Mimi Vo <mimzy414@gmail.com>
2021-04-21 12:26:36 -0500

    removed quesions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,57 @@ let startScore = 60;
 let timerEl = document.querySelector('#timer0');
 let scoreEl = document.querySelector('#score0');
 
+
+// Quiz questions, choices and answer key
+
+const questions = [
+    {
+        question: "Which of the following is true about variable naming conventions in JavaScript?",
+        choices: [
+            {choice1: " You should not use any of the JavaScript reserved keyword as variable name.", answer: false},
+            {choice2: " JavaScript variable names should not start with a numeral (0-9).", answer: false},
+            {choice3: " Both of the above", answer: true},
+            {choice4: " None of the above", answer: false},
+        ],
+    },
+    {
+        question: "How many times did Mimi want to cry during this build?",
+        choices: [
+            {choice1: " Never", answer: false},
+            {choice2: " Every single night over the past week",answer: true},
+            {choice3: " 1-2 times tops", answer: false},
+            {choice4: " None of the above...", answer: false}, 
+        ],
+    },
+    {
+        question: "What are variables used for in JavaScript Programs?",
+        choices: [
+            {choice1: " Storing numbers, dates, or other values", answer: true}, 
+            {choice2: " Varying randomly", answer: false}, 
+            {choice3: " Causing high-school algebra flashbacks", answer: false}, 
+            {choice4: " None of the above", answer: false}, 
+        ],
+    },
+    {
+        question: "Which built-in method returns the string representation of the number's value?",
+        choices: [
+            {choice1: " toValue()", answer: false}, 
+            {choice2: " toNumber()", answer: false}, 
+            {choice3: " toString()", answer: true}, 
+            {choice4: " None of the above", answer: false}, 
+        ],
+    },
+    {
+        question: "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
+        choices: [
+            {choice1:  " unshift()", answer: true},
+            {choice2:  " sort()", answer: false},
+            {choice3:  " splice()", answer: false},
+            {choice4:  " toString()", answer: false},
+        ],
+    },
+]
+
 //User selects Start to begin game, timer and Q/As appear on pageload
 function startGame() {
   randomQuestions = questions.sort(() => Math.random() - 0.5);
@@ -57,9 +108,14 @@ function showQuestion(question) {
 
 //Confirm Answer
     function confirmAnswer() {
+
+    selectAnswer();
 }
 //When a user selects a correct answer, do nothing
-    function selectAnswer() {}
+    function selectAnswer() {
+        
+        nextQuestion();
+    }
 
 //If a user selects an incorrect answer, subtract 5 seconds off the clock
 
