5162d354b6311e82941601b7e33439914e58cc37
Mimi Vo <mimzy414@gmail.com>
2021-04-21 18:01:03 -0500

    as good as it gets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
+
+
 //Start Game variables
 const gameStartEl = document.querySelector('.gamestartBtn');
+
 //const questionContainerEl = document.querySelector('question-container')
 const questionEl = document.querySelector('#gameQuestions');
 const choiceEL = document.querySelector('choice-btns');
 let randomQuestions, currentQuesIn;
 
+// time related variables 
 let timeSeconds = 60;
 let startScore = 60;
 let timerEl = document.querySelector('#timer0');
 let scoreEl = document.querySelector('#score0');
+var countDown;
+var sec;
+
+// local storage
+let myStorage = window.localStorage;
 
 
 // Quiz questions, choices and answer key
@@ -17,108 +26,138 @@ const questions = [
     {
         question: "Which of the following is true about variable naming conventions in JavaScript?",
         choices: [
-            {text: " You should not use any of the JavaScript reserved keyword as variable name.", answer: false},
-            {text: " JavaScript variable names should not start with a numeral (0-9).", answer: false},
-            {text: " Both of the above", answer: true},
-            {text: " None of the above", answer: false},
+            {choice: " You should not use any of the JavaScript reserved keyword as variable name.", answer: false},
+            {choice: " JavaScript variable names should not start with a numeral (0-9).", answer: false},
+            {choice: " Both of the above", answer: true},
+            {choice: " None of the above", answer: false},
         ],
     },
     {
         question: "How many times did Mimi want to cry during this build?",
         choices: [
-            {text: " Never", answer: false},
-            {text: " Every single night over the past week",answer: true},
-            {text: " 1-2 times tops", answer: false},
-            {text: " None of the above...", answer: false}, 
+            {choice: " Never", answer: false},
+            {choice: " Every single night over the past week",answer: true},
+            {choice: " 1-2 times tops", answer: false},
+            {choice: " None of the above...", answer: false}, 
         ],
     },
     {
         question: "What are variables used for in JavaScript Programs?",
         choices: [
-            {text: " Storing numbers, dates, or other values", answer: true}, 
-            {text: " Varying randomly", answer: false}, 
-            {text: " Causing high-school algebra flashbacks", answer: false}, 
-            {text: " None of the above", answer: false}, 
+            {choice: " Storing numbers, dates, or other values", answer: true}, 
+            {choice: " Varying randomly", answer: false}, 
+            {choice: " Causing high-school algebra flashbacks", answer: false}, 
+            {choice: " None of the above", answer: false}, 
         ],
     },
     {
         question: "Which built-in method returns the string representation of the number's value?",
         choices: [
-            {text: " toValue()", answer: false}, 
-            {text: " toNumber()", answer: false}, 
-            {text: " toString()", answer: true}, 
-            {text: " None of the above", answer: false}, 
+            {choice: " toValue()", answer: false}, 
+            {choice: " toNumber()", answer: false}, 
+            {choice: " toString()", answer: true}, 
+            {choice: " None of the above", answer: false}, 
         ],
     },
     {
         question: "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
         choices: [
-            {text:  " unshift()", answer: true},
-            {text:  " sort()", answer: false},
-            {text:  " splice()", answer: false},
-            {text:  " toString()", answer: false},
+            {choice:  " unshift()", answer: true},
+            {choice:  " sort()", answer: false},
+            {choice:  " splice()", answer: false},
+            {choice:  " toString()", answer: false},
         ],
     },
 ]
 
 //User selects Start to begin game, timer and Q/As appear on pageload
 function startGame() {
-  randomQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuesIn = 0;
-  nextQuestion();
-}
-//Timer kicks off with 60 seconds
+
+    //Timer kicks off with 60 seconds
     scoreEl.innerHTML = `${timeSeconds}`;
     timerEl.innerHTML = `00:${timeSeconds}`;
-    const countDown = setInterval(() => {
-    timeSeconds--;
-        displayTime(timeSeconds);
-    if (timeSeconds <= 0 || timeSeconds < 1) {
-        clearInterval(countDown);
-  }
-}, 1000);
+    countDown = setInterval(() => {
+       timeSeconds--;
+           displayTime(timeSeconds);
+       if (timeSeconds <= 0 || timeSeconds < 1) {
+           clearInterval(countDown);
+     }
+    }, 1000);
+
+    // get questions
+    randomQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuesIn = 0;
+        showQuestion(randomQuestions[currentQuesIn]);
+    
+    }
 
 function displayTime(second) {
     const min = Math.floor(second / 60);
-    const sec = Math.floor(second % 60);
+    sec = Math.floor(second % 60);
     timerEl.innerHTML = `${min < 10 ? '0' : ''}${min}:${sec}`;
     scoreEl.innerHTML = `${sec}`;
-}
-
-//Display random question and answer choices
-function nextQuestion() {
-  showQuestion(randomQuestions[currentQuesIn]);
-}
-
-//Pull in question from array
-function showQuestion(question) {
-    console.log(question);
-    questionEl.innerText = question.question;
-    question.choice.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click')
-        answerEl.appendChild(button)
-      })
-  }
-
-//Confirm Answer
-    function confirmAnswer() {
-
-    selectAnswer();
-}
-//When a user selects a correct answer, do nothing
-    function selectAnswer() {
-        
-        nextQuestion();
     }
 
-//If a user selects an incorrect answer, subtract 5 seconds off the clock
+//Pull in question from array and show on screen
+function showQuestion(randomQuestion) {
+    let val = 1
+    
+    questionEl.innerText = randomQuestion.question;
 
-//On Start Button click, Game begins
-startGame();
+    randomQuestion.choices.forEach(optionSelect => {
+        // const button = document.createElement('button')
+        let choiceHtml = "#choice_" + String(val)
+        let button = document.querySelector(choiceHtml);
+        button.innerText = optionSelect.choice
+        val++
+      })
+    }
+
+// On selection of an answer, confirm if right and move on to the next question
+function selectAnswer(chosen) {
+        if(currentQuesIn < randomQuestions.length-1){
+            let chosenAnswer_id = Number(chosen.split("_")[1]) - 1
+            let chosenAnswer_TF = randomQuestions[currentQuesIn].choices[chosenAnswer_id].answer
+            if (chosenAnswer_TF != true){
+                // subtract time
+                timeSeconds -= 5
+            }
+            
+            currentQuesIn++
+            showQuestion(randomQuestions[currentQuesIn]);
+        } else{
+            // end game
+            
+            clearInterval(countDown) // stops your time
+            let initials = window.prompt("Please input your initials to save your high score!");
+            let time = sec
+            let highScores = [initials, time]
+
+            localStorage.setItem('intial', initials)
+            localStorage.setItem('score', time.toString())
+            console.log(localStorage.getItem('intial'))
+            console.log(localStorage.getItem('score'))
+
+            
+            window.alert("Hello! "+ highScores[0] + ", your score is " + highScores[1])
+
+        }
+        
+    }
+
+
+
+
+
+try {
+    // high score elements
+    const scoreNameEl = document.querySelector('#scoreName');
+    const finalscoreEl = document.querySelector('#scoreNumber');
+    scoreNameEl.innerText = localStorage.getItem('intial')
+    finalscoreEl.innerText = localStorage.getItem('score')
+  } catch (error) {
+    console.error(error);
+    startGame()
+  }
+
 
